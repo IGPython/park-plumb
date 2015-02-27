007dd964f9b3b6681e245b9ec81510a0d385f454
Rhys Park <sales@igpython.co.uk>
2015-02-27 00:09:54 +0000

    Remove PHP Short Tags

--- includes/slider.php
+++ includes/slider.php
@@ -1,11 +1,4 @@
 
-<?
-/* 
-Image banner jsquery 
-revised by Rhys Park 
-
-*/
-?>
 <div class="wrapper row2">
   <div id="container" class="clear">
     <!-- Slider -->
@@ -14,34 +7,34 @@ revised by Rhys Park
       <div id="slider">
         <ul>
           <li>
-            <figure><img src="<? echo image1?>" alt="<? echo image1_name ?>" />
+            <figure><img src="<?php echo image1?>" alt="<?php echo image1_name ?>" />
               <figcaption>
-                <h2><?echo image1_title ?></h2>
-                <p><? echo image1_desc ?></p>
+                <h2><?php echo image1_title ?></h2>
+                <p><?php echo image1_desc ?></p>
               </figcaption>
             </figure>
           </li>
           <li>
-            <figure><img src="<? echo image2 ?>" alt="<? echo image2_name ?>" />
+            <figure><img src="<?php echo image2 ?>" alt="<?php echo image2_name ?>" />
               <figcaption>
-                <h2><?echo image2_title ?></h2>
-                <p><?echo image2_desc ?></p>
+                <h2><?php echo image2_title ?></h2>
+                <p><?php echo image2_desc ?></p>
               </figcaption>
             </figure>
           </li>
           <li>
-            <figure><img src="<? echo image3 ?>" alt="<? echo image3_name?>" />
+            <figure><img src="<?php echo image3 ?>" alt="<?php echo image3_name?>" />
               <figcaption>
-                <h2><?echo image3_title ?></h2>
-                <p><?echo image3_desc ?></p>
+                <h2><?php echo image3_title ?></h2>
+                <p><?php echo image3_desc ?></p>
                 <?php // <footer class="more"><a href="#">Read More &raquo;</a></footer>   REMOVED?>
               </figcaption>
             </figure>
 			<li>
-	            <figure><img src="<? echo image4 ?>" alt=" <? echo image4_name?>" />
+	            <figure><img src="<?php echo image4 ?>" alt=" <?php echo image4_name?>" />
 	              <figcaption>
-	                <h2><?echo image4_title ?></h2>
-	                <p><?echo image4_desc?> </p>
+	                <h2><?php echo image4_title ?></h2>
+	                <p><?php echo image4_desc?> </p>
 	              </figcaption>
 	            </figure>
 	          </li>
